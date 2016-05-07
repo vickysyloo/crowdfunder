f5bc54ffb56f2fabd3ca2fe1a88602a246cd0586
Vicky L <vicky.sy.loo@gmail.com>
2016-05-07 15:19:38 -0400

    fixed bug in user controller create

--- crowdfunder/app/controllers/users_controller.rb
+++ crowdfunder/app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:alert] = "Successfully signed up!"
-      auto_login(user)
+      auto_login(@user)
       redirect_to root_path
     else
       render :new
